3d39eaabecbab241ac90868b194e2c615b0acf00
Jake Gordon <github@jakeg.co.uk>
2018-10-28 20:21:21 +0000

    optimise problem 4

--- 4.js
+++ 4.js
@@ -5,18 +5,17 @@ JG.solution(4, () => {
   Find the largest palindrome made from the product of two 3-digit numbers.
   END */
 
-  // const products = {} // TODO: use a hash map and memoise
   function maxPalindromeProduct (digits) {
     let max = 0
     const maxInput = Number('9'.repeat(digits))
     const minInput = Number(`1${'0'.repeat(digits - 1)}`)
     for (let i = maxInput; i >= minInput; i--) {
-      for (let j = maxInput; j >= minInput; j--) {
+      // opt: j >= i so we avoid duplicate products; saves ~50%
+      for (let j = maxInput; j >= minInput && j >= i; j--) {
         const product = i * j
+        // opt: massive improvement with product > max to avoid isPalindrome most of the time
         if (product > max) {
-          if (isPalindrome(product)) {
-            max = product
-          }
+          if (isPalindrome(product)) max = product
         }
       }
     }
@@ -24,9 +23,8 @@ JG.solution(4, () => {
   }
 
   function isPalindrome (str) {
-    // first the base case
     str = String(str)
-    if (str.length <= 1) return true
+    if (str.length <= 1) return true // base case
     if (str[0] !== str[str.length - 1]) return false
     return isPalindrome(str.slice(1, str.length - 1))
   }
