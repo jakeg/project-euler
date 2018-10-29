fcbf472e3fce4f98472973561e37af93105175f7
Jake Gordon <github@jakeg.co.uk>
2018-10-29 12:36:24 +0000

    16: work with any base

--- 16.js
+++ 16.js
@@ -5,24 +5,27 @@ JG.solution(16, () => {
   What is the sum of the digits of the number 2<sup>1000</sup>?
   END */
 
-  const max = 1000
-  const digits = [1] // use an array as 64bit float isn't enough; left to right
-  for (let pow = 1; pow <= max; pow++) {
-    // we're going to multiply each digit by 2
-    // like on paper with long addition, carrying numbers across
-    let carry = 0
-    for (let i = 0; i < digits.length; i++) {
-      const double = String(digits[i] * 2 + carry).split('')
-      digits[i] = Number(double.pop())
-      carry = Number(double.join(''))
+  function powerDigits (base, power) {
+    // uses array as 64bit float isn't enough; left to right
+    const digits = [1]
+    for (let pow = 1; pow <= power; pow++) {
+      // we're going to multiply each digit by the base
+      // like paper long addition, carrying numbers across
+      let carry = 0
+      for (let i = 0; i < digits.length; i++) {
+        const multiplied = String(digits[i] * base + carry).split('')
+        digits[i] = Number(multiplied.pop())
+        carry = Number(multiplied.join(''))
+      }
+      if (carry) {
+        // would be simpler if just base 2 but making this work to any base
+        String(carry).split('').reverse().forEach(v => digits.push(Number(v)))
+      }
     }
-    if (carry) {
-      // would be simpler if just base 2 but makign this work to any base
-      String(carry).split('').reverse().forEach(v => digits.push(Number(v)))
-    }
+    return digits
   }
 
-  const sum = digits.reduce((acc, v) => acc + v, 0)
+  const sum = powerDigits(2, 1000).reduce((acc, v) => acc + v, 0)
 
   return sum
 })
