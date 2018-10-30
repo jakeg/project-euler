b38f19da166d8de6a596a02ac43e17a95b5ba3ef
Jake Gordon <github@jakeg.co.uk>
2018-10-30 10:30:05 +0000

    big files can be external resources

--- script.js
+++ script.js
@@ -4,41 +4,62 @@ JG = (function (jg) {
   const problemNum = document.location.search.substr(1)
 
   if (problemNum) {
-    const script = document.createElement('script')
-    script.setAttribute('src', `${problemNum}.js`)
-    document.body.appendChild(script)
+    addSrc(`${problemNum}.js`)
   } else {
     const title = document.createTextNode('Append ?1 to URL for my solution to https://projecteuler.net/problem=1 etc')
     document.body.appendChild(title)
   }
 
-  function solution (num, func) {
-    // separate the comment explaining the problem from the solution
-    const tmp = document.createElement('p')
-    tmp.innerText = func
-    const [ code1, problemText, code2 ] = tmp.innerText.split(/ *\/\* *START| *END *\*\/\n/m) // use /*START blah blah END*/ to hold the problem text
-    const codeText = [code1, code2].join('')
+  function solution (num, func, file) {
+    if (file) addSrc(file, run)
+    else run()
 
-    const title = document.createElement('h1')
-    title.innerHTML = `Project Euler - <a href="https://projecteuler.net/problem=${num}">Problem #${num}</a>`
-    document.body.appendChild(title)
+    function run () {
+      // separate the comment explaining the problem from the solution
+      const tmp = el('p')
+      tmp.innerText = func
+      const [ code1, problemText, code2 ] = tmp.innerText.split(/ *\/\* *START| *END *\*\/\n/m) // use /*START blah blah END*/ to hold the problem text
+      const codeText = [code1, code2].join('')
 
-    const problem = document.createElement('div')
-    const text = problemText.split('\n').map(line => line.trim()).join('<br>').trim()
-    problem.innerHTML = `<p>${text}</p>`
-    document.body.appendChild(problem)
+      const title = el('h1')
+      title.innerHTML = `Project Euler - <a href="https://projecteuler.net/problem=${num}">Problem #${num}</a>`
+      document.body.appendChild(title)
 
-    const result = document.createElement('p')
-    const start = Date.now()
-    let evaled = func()
-    if (Array.isArray(evaled)) evaled = evaled.join(', ')
-    const timeTaken = Date.now() - start
-    result.innerHTML = `Result: <b>${evaled}</b> (took ${timeTaken}ms)`
-    document.body.appendChild(result)
+      const problem = el('div')
+      const text = problemText.split('\n').map(line => line.trim()).join('<br>').trim()
+      problem.innerHTML = `<p>${text}</p>`
+      document.body.appendChild(problem)
 
-    const p = document.createElement('pre')
-    p.innerText = codeText
-    document.body.appendChild(p)
+      if (file) {
+        const withFile = el('p')
+        withFile.innerHTML = `<i>Using file <a href="${file}">${file}</a></i>`
+        document.body.appendChild(withFile)
+      }
+
+      const result = el('p')
+      const start = Date.now()
+      let evaled = func()
+      if (Array.isArray(evaled)) evaled = evaled.join(', ')
+      const timeTaken = Date.now() - start
+      result.innerHTML = `Result: <b>${evaled}</b> (took ${timeTaken}ms)`
+      document.body.appendChild(result)
+
+      const p = el('pre')
+      p.innerText = codeText
+      document.body.appendChild(p)
+    }
+  }
+
+  // can't use xhr if working on file://
+  function addSrc (path, cb) {
+    const script = el('script')
+    if (cb) script.onload = cb
+    script.src = path
+    document.body.appendChild(script)
+  }
+
+  function el (el) {
+    return document.createElement(el)
   }
 
   jg.solution = solution
